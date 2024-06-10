f8f953f662d3b394722f3a570d659452abdb24d3
Tomi Helin <tomi.helin@student.lut.fi>
2024-06-10 19:27:25 +0300

    Tasks 1 and 2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var todoRouter = require('./routes/todo');
+var todosRouter = require('./routes/todo');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/todo', todoRouter);
+app.use('/todo', todosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/todo.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+let todos = [];
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+    console.log("todo function");
+    console.log(req.body);
+    const username = req.body['user-name'];
+    const task = req.body['input-task'];
+  
+    console.log(username);
+    console.log(task);
+
+    const keys = todos.keys();
+
+    const userObject = todos.find(item => item.name === username);
+    if (userObject) {
+        userObject.todos.push(task);
+        res.send('Todo added');
+    }
+    else {
+        const newUser = {
+            name: username,
+            todos: [task]
+        };
+        todos.push(newUser);
+        res.send('User added');
+    }
+});
+
+module.exports = router;
